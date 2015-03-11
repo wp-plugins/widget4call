97b360840f9f64f416684dc90edf0462f6af927d
apidaze <apidaze@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-11 20:08:47 +0000

    message pa form
    git-svn-id: https://plugins.svn.wordpress.org/widget4call/trunk@1110596 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- widget4call.php
+++ widget4call.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Widget4Call
 Description: Click to Call and Web Call Back solution By APIdaze
-Version: 1.0.4
+Version: 1.0.5
 Author: APIdaze
 Author URI: https://developers.apidaze.io
 */
